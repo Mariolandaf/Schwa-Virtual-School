19e88bd0735c58999e12be54e424a4e21e0cbad5
Mariolandaf <mariolandaf@hotmail.com>
2023-02-01 15:34:30 -0600

    Add files via upload

---
+++ assets/js/sweet.js
@@ -0,0 +1,10 @@
+function contacto() {
+    Swal.fire ({
+    title: "Mario Landa",
+    text: "Email: mariolandaf2@gmail.com",
+    icon: "info",
+    toast: true,
+    position: "top",
+    confirmButtonColor: "#1f628e",
+})
+}
